ddb02a03950108d3dea6bdd7cbbdd535406e60ee
Ryan Gomoto <ryan@cyrusbio.com>
2019-02-10 19:30:27 -0800

    List points in control panel

--- src/ControlPanel/ControlPanel.tsx
+++ src/ControlPanel/ControlPanel.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { List } from 'antd';
 import './ControlPanel.css';
 import { CartesianObject } from '../state';
 
@@ -16,6 +17,29 @@ export class ControlPanel extends React.Component<ControlPanelProps> {
     return (
       <div className="ControlPanel">
         <div>{numberOfPoints} point{numberOfPoints === 1 ? '' : 's'}</div>
+        <List
+          itemLayout="horizontal"
+          dataSource={this.props.objects}
+          renderItem={(o: CartesianObject) => {
+            let objectSpecificContent: React.ReactNode;
+            switch (o.objectType) {
+              case 'point': {
+                objectSpecificContent = (
+                  <div>
+                    <span>Point</span>
+                    <span>{o.position.x}, {o.position.y}, {o.position.z}</span>
+                  </div>
+                );
+                break;
+              }
+            }
+            return (
+              <List.Item>
+                {objectSpecificContent}
+              </List.Item>
+            );
+          }}
+        />
         <button onClick={this.props.onAddPoint.bind(this)}>Add point</button>
         <button onClick={this.props.onAddManyPoints.bind(this)}>Add 3 objects</button>
         <button onClick={this.props.onRemovePoint.bind(this)}>Remove point</button>
