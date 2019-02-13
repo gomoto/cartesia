fe00d6692c7713a2055658bb4fa1e5f778fc6a06
Ryan Gomoto <ryan@cyrusbio.com>
2019-02-13 01:47:56 -0800

    Delete selected objects using backspace or del

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,15 @@ import { reducer } from './reducer';
 import { Provider } from 'react-redux';
 import { ControlPanelContainer } from './ControlPanel/ControlPanelContainer';
 import { ViewerContainer } from './Viewer/ViewerContainer';
+import mousetrap from 'mousetrap';
 import './index.css';
+import { RemoveSelectedObjectsAction } from './actions';
 
 const store = createStore(undoable(reducer));
+
+mousetrap.bind(['backspace', 'del'], () => {
+  store.dispatch<RemoveSelectedObjectsAction>({type: 'REMOVE_SELECTED_OBJECTS'})
+});
 
 ReactDOM.render((
   <Provider store={store}>
