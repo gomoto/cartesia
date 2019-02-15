0851ee71a49186240ad61d25120b630bcccc213f
Ryan Gomoto <ryan@cyrusbio.com>
2019-02-14 20:23:46 -0800

    Center grid lines around 0 in all three dimensions

--- src/Viewer/create-grid.ts
+++ src/Viewer/create-grid.ts
@@ -20,26 +20,49 @@ export function createGrid(scene: BABYLON.Scene, grid: CartesianGrid): BABYLON.L
 
   const lines: [BABYLON.Vector3, BABYLON.Vector3][] = [];
 
-  // x → y → z
-  for (let x = xMin; x <= xMax; x+=xStep) {
-    for (let y = yMin; y <= yMax; y+=yStep) {
-      lines.push([new BABYLON.Vector3(x, y, zMin), new BABYLON.Vector3(x, y, zMax)]);
-    }
+  // Grid lines should be centered around 0 in all three dimensions
+  const xs: number[] = [];
+  const ys: number[] = [];
+  const zs: number[] = [];
+
+  // x values
+  const xMinRounded = Math.ceil(xMin / xStep) * xStep; // first x value greater than xMin
+  for (let x = xMinRounded; x <= xMax; x += xStep) {
+    xs.push(x);
   }
+
+  // y values
+  const yMinRounded = Math.ceil(yMin / yStep) * yStep; // first y value greater than yMin
+  for (let y = yMinRounded; y <= yMax; y += yStep) {
+    ys.push(y);
+  }
+
+  // z values
+  const zMinRounded = Math.ceil(zMin / zStep) * zStep; // first z value greater than zMin
+  for (let z = zMinRounded; z <= zMax; z += zStep) {
+    zs.push(z);
+  }
+
+  // x → y → z
+  xs.forEach((x) => {
+    ys.forEach((y) => {
+      lines.push([new BABYLON.Vector3(x, y, zMin), new BABYLON.Vector3(x, y, zMax)]);
+    });
+  });
 
   // y → z → x
-  for (let y = yMin; y <= yMax; y+=yStep) {
-    for (let z = zMin; z <= zMax; z+=zStep) {
+  ys.forEach((y) => {
+    zs.forEach((z) => {
       lines.push([new BABYLON.Vector3(xMin, y, z), new BABYLON.Vector3(xMax, y, z)]);
-    }
-  }
+    });
+  });
 
   // z → x → y
-  for (let z = zMin; z <= zMax; z+=zStep) {
-    for (let x = xMin; x <= xMax; x+=xStep) {
+  zs.forEach((z) => {
+    xs.forEach((x) => {
       lines.push([new BABYLON.Vector3(x, yMin, z), new BABYLON.Vector3(x, yMax, z)]);
-    }
-  }
+    });
+  });
 
   const gridMesh = BABYLON.MeshBuilder.CreateLineSystem('grid', {
     lines,
